cb3d5b316723e3070819b9f7528291e11474e3e5
vasiliy.y <shersticklock@gmail.com>
2019-05-17 10:54:45 +0300

    add

--- python/Pandas+numpy+scikitlearn/Forecast/forecast.py
+++ python/Pandas+numpy+scikitlearn/Forecast/forecast.py
@@ -6,13 +6,14 @@ from sqlalchemy import create_engine
 import joblib
 
 class Forecast():
-    def __init__(self, csv_path, model):
+    def __init__(self, csv_path, model_path, con_db):
         self.path = csv_path
-        self.model = model
+        self.model = model_path
+        self.con = con_db
 
     def conn_to_db(self):
         """Подключение к базе"""
-        conn = create_engine('postgresql+psycopg2://postgresadmin:admin123@192.168.99.100:32543/postgresdb')
+        conn = create_engine(self.con)
         return conn
 
     def load_data_from_csv(self):
@@ -22,22 +23,23 @@ class Forecast():
         data.to_sql(name="SOURCE", con=con, if_exists='replace')
 
     def read_data_from_db(self):
-        """ Чтение данных из БД"""
+        """Чтение данных из БД"""
         con = self.conn_to_db()
         data = read_sql('SELECT * FROM public."SOURCE"', con)
         return data
+
     def load_data_to_db(self,data):
         """Загрузка прогноза в базу FORECAST"""
-        fc = pd.Series(data=data)
+        fc = pd.Series(data=data,name='forecast')
         con = self.conn_to_db()
         fc.to_sql(name="FORECAST", con=con, if_exists='replace')
         print(fc)
+
     def forecast(self, data):
+        """Предсказание меток по уже обученной модели"""
         loaded_model = joblib.load(self.model)
         pred_1 = loaded_model.predict(data)
         self.load_data_to_db(pred_1)
-        #joblib.dump(pred_1, 'forecast.pkl')
-
 
     def main(self):
         self.load_data_from_csv()
@@ -46,5 +48,6 @@ class Forecast():
         self.forecast(df)
 
 if __name__ == '__main__':
-    fc = Forecast("C:\kute\csv.csv", "C:\kute\model.sav")
+    fc = Forecast(csv_path="csv.csv", model_path="model.sav",
+                  con_db='postgresql+psycopg2://postgresadmin:admin123@192.168.99.100:32543/postgresdb')
     fc.main()
