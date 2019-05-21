2905a234bd4c6463428fb817490427ae3e2229fb
vasiliy.y <shersticklock@gmail.com>
2019-05-21 14:45:23 +0300

    add service

---
+++ python/Pandas+numpy+scikitlearn/Forecast/rest_service.py
@@ -0,0 +1,16 @@
+from flask import Flask, make_response, request
+import forecast
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def index():
+    fc = forecast.Forecasting(request.json, model_path="model.sav",
+                              con_db='postgresql+psycopg2://postgresadmin:admin123@192.168.99.100:32543/postgresdb')
+    resp = make_response(fc.start(),200)
+    resp.headers['Content-Type'] = 'text/json'
+    return resp
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
